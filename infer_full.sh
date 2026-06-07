a3e3ef29ae901839ebe713325c23cdb1be1172f4
spawner1145 <spawner6@outlook.com>
2026-06-07 11:34:54 +0800

    sh脚本

--- infer_full.sh
+++ infer_full.sh
@@ -1,19 +1,18 @@
-# 开--no_reference_sequence是因为训练没带latent特征
-
 python anima_minimal_inference.py \
   --dit /root/autodl-tmp/ComfyUI/models/diffusion_models/anima-base-v1.0.safetensors \
   --vae /root/autodl-tmp/ComfyUI/models/vae/qwen_image_vae.safetensors \
   --text_encoder /root/autodl-tmp/ckn/qwen3 \
-  --t5_tokenizer_path configs/t5_old \
-  --prompt "your prompt here" \
-  --reference_image /root/autodl-tmp/ckn/test/foo.png \
+  --prompt "你的提示词" \
   --image_size 1024 1024 \
-  --infer_steps 30 \
-  --guidance_scale 3.5 \
+  --reference_image /root/autodl-tmp/ckn/test/ref.png \
+  --save_path out/infer.png \
+  --lora_weight out/anima_multi_ipadapter_self_ref_overfit-step00000500.safetensors \
+  --lora_multiplier 1.0 \
   --ip_adapter \
-  --ip_adapter_weight out/anima_ipadapter_self_ref_overfit-000000500_ip_adapter.safetensors \
+  --linear-adapter \
+  --ip_adapter_weight out/anima_multi_ipadapter_self_ref_overfit-step00000500_ip_adapter.safetensors \
   --ip_adapter_feature_backend ccip \
   --ip_adapter_feature_model /root/autodl-tmp/ckn/ccip_model \
   --ip_adapter_num_tokens 8 \
-  --save_path outputs \
-  --no_reference_sequence \
+  --infer_steps 50 \
+  --guidance_scale 3.5
---
+++ infer_ipa.sh
@@ -0,0 +1,19 @@
+python anima_minimal_inference.py \
+  --dit /root/autodl-tmp/ComfyUI/models/diffusion_models/anima-base-v1.0.safetensors \
+  --vae /root/autodl-tmp/ComfyUI/models/vae/qwen_image_vae.safetensors \
+  --text_encoder /root/autodl-tmp/ckn/qwen3 \
+  --prompt "1girl, blue background, commentary, facing to the side, finger to face, highres, looking at viewer, makoto mirai academy high school uniform, pointing, pointing up, translation request, upper body, yano akane" \
+  --image_size 1024 1024 \
+  --reference_image /root/autodl-tmp/ckn/test/0d4634a46bab35b9e310dacd350e15d8.jpg \
+  --save_path out/infer.png \
+  --lora_weight out/anima_ipadapter_self_ref_overfit-step00028500.safetensors \
+  --lora_multiplier 1.0 \
+  --ip_adapter \
+  --linear-adapter \
+  --ip_adapter_weight out/anima_ipadapter_self_ref_overfit-step00028500_ip_adapter.safetensors \
+  --ip_adapter_feature_backend ccip \
+  --ip_adapter_feature_model /root/autodl-tmp/ckn/ccip_model \
+  --ip_adapter_num_tokens 8 \
+  --no_reference_sequence \
+  --infer_steps 30 \
+  --guidance_scale 3.5
